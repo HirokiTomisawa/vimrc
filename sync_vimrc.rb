2fe4c7a58bf9b12dc38bdac14ec9dce4970693fa
HirokiTomisawa <47888809+HirokiTomisawa@users.noreply.github.com>
2019-06-02 17:03:13 +0900

    不要ファイルの削除

--- sync_vimrc.rb
+++
@@ -1,8 +0,0 @@
-# gitで管理したいため, macbookのホームに保存されている.vimrc ファイルを同期
-#
-# 本体のファイル
-# /User/Buchi/.vimrc
-# gitへ同期されるファイル
-# /User/Buchi/Documents/ProgrammingFolder/vimrc/.vimrc
-
-puts `cp -f ~/.vimrc ~/Documents/ProgrammingFolder/vimrc/`
